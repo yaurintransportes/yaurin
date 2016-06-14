3519e107815156d6886e85fe23823b7110f3e435
yaurintransportes <mariacolon@pivotal-guest-7-88.ny.pivotallabs.com>
2016-06-14 18:59:08 -0400

    style email link

--- source/javascripts/contact.js
+++ source/javascripts/contact.js
@@ -6,12 +6,14 @@ $(function() {
 $(function() {
 	var contacts = [{
 		label: "Contact1",
-		text: "do some contacting"
+		text: "Hablanos como te ayudamos  para un mejor servicio en tu progreso para enviar Rapido Directo Confiable"
 	},
 	{
-		label: "Furgonetas de 500kg",
-		text: "Furgonetas de 500kg"
+		label: "Email",
+		text: "<h1 class='email'>Email</h1><div>Email us with as much details as you can so we can give you the best help possible</div> <div class='contact-me'><a href='mailto:youremailaddress'>Email Me</a></div>"
 	}];
 
 	initializeSelectableList(contacts, "contact-list");
 })
+
+// <p attribute1="something" another-attribute="another-thing"></p>
